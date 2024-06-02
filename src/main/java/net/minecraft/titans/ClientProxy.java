a7db196c465490a5d87aa629fd10707f693af5a2
theZWbutDisconnected <123157349+ZerWhit@users.noreply.github.com>
2024-06-02 22:05:00 +0800

    Render ZombieVariant was updated

--- src/main/java/net/minecraft/titans/ClientProxy.java
+++ src/main/java/net/minecraft/titans/ClientProxy.java
@@ -7,10 +7,15 @@ import net.minecraft.titans.client.entity.renders.RenderWitherzilla;
 import net.minecraft.titans.entity.EntityGammaLightning;
 import net.minecraft.titans.entity.EntityUrLightning;
 import net.minecraft.titans.entity.god.EntityWitherzilla;
+import net.minecraft.titans.entity.monster.EntityZombieVariant;
+import net.minecraft.titans.entity.render.RenderZombieVariant;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy extends CommonProxy
 {
@@ -22,6 +27,7 @@ public class ClientProxy extends CommonProxy
 		super.preInit(e);
 		manager = new TitanManagerClient();
 		renderEntities();
+		MinecraftForge.EVENT_BUS.register(ClientProxy.class);
 	}
 
 	@Override
@@ -35,9 +41,15 @@ public class ClientProxy extends CommonProxy
 	{
 		super.postInit(e);
 	}
-	
+
+	@SubscribeEvent
+	public static void renderItems(ModelRegistryEvent event) {
+		//ModelHelper.render(TItems.ultimaBlade, new ModelUltimaBlade(), "textures/entities/ultima_blade");
+	}
+
 	public void renderEntities()
 	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityZombieVariant.class, manager -> new RenderZombieVariant(manager));
 		RenderingRegistry.registerEntityRenderingHandler(EntityWitherzilla.class, manager -> new RenderWitherzilla(manager));
 		RenderingRegistry.registerEntityRenderingHandler(EntityGammaLightning.class, manager -> new RenderGammaLightning(manager));
 		RenderingRegistry.registerEntityRenderingHandler(EntityUrLightning.class, manager -> new RenderUrLightning(manager));
